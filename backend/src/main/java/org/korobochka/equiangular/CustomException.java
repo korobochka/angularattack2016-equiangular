09f38cf763e918e3d2c7fd6d56098df12b3aa987
korobochka <korobochka191@gmail.com>
2016-05-14 12:29:08 +0300

    Add exception handling

---
+++ backend/src/main/java/org/korobochka/equiangular/CustomException.java
@@ -0,0 +1,13 @@
+package org.korobochka.equiangular;
+
+/**
+ * Created by korobochka on 5/14/16.
+ */
+public class CustomException extends Exception {
+	public CustomException(String msg) {
+		super(msg);
+	}
+	public CustomException(String msg, Throwable reason) {
+		super(msg, reason);
+	}
+}
--- backend/src/main/java/org/korobochka/equiangular/Main.java
+++ backend/src/main/java/org/korobochka/equiangular/Main.java
@@ -1,7 +1,13 @@
 package org.korobochka.equiangular;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import org.eclipse.jetty.http.HttpStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Modifier;
 
 import static spark.Spark.*;
 
@@ -10,9 +16,27 @@ import static spark.Spark.*;
  */
 public class Main {
 	private static final Logger log = LoggerFactory.getLogger(Main.class);
+	private static final Gson gson = new GsonBuilder()
+			.excludeFieldsWithModifiers(Modifier.STATIC, Modifier.TRANSIENT, Modifier.VOLATILE)
+			.create();
 
 	public static void main(String[] args) {
 		enableCORS("*.2016.angularattack.io", "POST, GET, DELETE, PUT, OPTIONS", "Content-Type");
+
+		exception(CustomException.class, (exception, request, response) -> {
+			response.status(HttpStatus.FORBIDDEN_403);
+			response.body(gson.toJson(exception));
+		});
+
+		exception(Exception.class, (exception, request, response) -> {
+			Throwable t = exception;
+			while(t != null) {
+				System.out.println(t.toString());
+				t = t.getCause();
+			}
+			response.status(HttpStatus.INTERNAL_SERVER_ERROR_500);
+			response.body(exception.toString());
+		});
 
 		before((request, response) -> request.attribute("timeStarted", System.currentTimeMillis()));
 		after((request, response) -> {
@@ -20,15 +44,22 @@ public class Main {
 			log.info(request.url() + " completed in " + timeSpent + " ms");
 		});
 
-		get("/", (req, res) -> {
-			return "Hello from the backend";
+		before("/api/*", (request, response) -> {
+			response.type("application/json");
 		});
+
+		get("/api/ping", (req, res) -> {
+			return "pong";
+		});
+
+		get("/api/throw", ((request, response) -> {
+			throw new CustomException("test reason");
+		}));
 	}
 
 	private static void enableCORS(final String origin, final String methods, final String headers) {
 
-		options("/*", (request, response) -> {
-
+		options("/api/*", (request, response) -> {
 			String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
 			if (accessControlRequestHeaders != null) {
 				response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
@@ -46,8 +77,6 @@ public class Main {
 			response.header("Access-Control-Allow-Origin", origin);
 			response.header("Access-Control-Request-Method", methods);
 			response.header("Access-Control-Allow-Headers", headers);
-			// Note: this may or may not be necessary in your particular application
-			response.type("application/json");
 		});
 	}
 }
