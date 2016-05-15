e366a115c318ceba7e4fe6f8566ae3c92f38817e
Andrey Miroshnichenko <a@avm.name>
2016-05-15 15:17:02 +0300

    App loading indicator (revised)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,3 @@
-/*
- * Angular 2 decorators and services
- */
 import { Component, ViewEncapsulation } from '@angular/core';
 import { RouteConfig, Router } from '@angular/router-deprecated';
 
@@ -9,7 +6,6 @@ import { RouterActive } from './router-active';
 
 /*
  * App Component
- * Top Level Component
  */
 @Component({
     selector: 'app',
@@ -70,7 +66,6 @@ import { RouterActive } from './router-active';
 @RouteConfig([
 //    { path: '/',      name: 'Index', component: Home, useAsDefault: true },
 //    { path: '/home',  name: 'Home',  component: Home },
-    // Async load a component using Webpack's require with es6-promise-loader and webpack `require`
     { path: '/', name: 'About', useAsDefault: true, loader: () => require('es6-promise!./pages/about/PageAboutComponent')('PageAboutComponent') },
     { path: '/test', name: 'Test', loader: () => require('es6-promise!./pages/test/PageTestComponent')('PageTestComponent') },
     { path: '/profile', name: 'Profile', loader: () => require('es6-promise!./pages/profile/PageProfileComponent')('PageProfileComponent') },
@@ -81,13 +76,9 @@ import { RouterActive } from './router-active';
 export class App {
     loading = false;
 
-    constructor(
-        public appState: AppState) {
-
+    constructor(public appState: AppState) {
     }
 
     ngOnInit() {
-        console.log('Initial App State', this.appState.state);
     }
-
 }
