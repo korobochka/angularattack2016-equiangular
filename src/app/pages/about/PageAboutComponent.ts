fa08be2c505f548f678114624239efa8bc7663c2
Andrey Miroshnichenko <a@avm.name>
2016-05-15 21:00:02 +0300

    About dialog texting

--- src/app/pages/about/PageAboutComponent.ts
+++ src/app/pages/about/PageAboutComponent.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'page-about',
     template: `
-    <h3 class="page-title">About Application</h3>
+    <h3 class="page-title">Equiangular: smarter testing</h3>
 
     <div class="mdl-grid">
         <div class="mdl-cell mdl-cell--12-col mdl-shadow--2dp static-page-content">
-            Few words about functions, how to install and so forth
+            <p>Our goal is to create a better platform for estimating personal skills.</p>
+            <p>Simply login with your LinkedIn account, add a few skills to your profile and then answer automatic quizzes and see your estimated knowledge on the profile page.</p>
         </div>
     </div>
   `
