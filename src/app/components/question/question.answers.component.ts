14a64479addfd6dc21adbbacb98ba9a0b23d819f
Andrey Miroshnichenko <a@avm.name>
2016-05-15 13:28:38 +0300

    Question display components rework to support multiply answer options and event handling

--- src/app/components/question/question.answers.component.ts
+++ src/app/components/question/question.answers.component.ts
@@ -1,16 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, ElementRef, ViewChildren } from '@angular/core';
+import { QuestionAnswerComponent } from './questions.answer.component';
 
 @Component({
     selector: 'question-answers',
-    inputs: [ 'answers' ],
+    inputs: [
+        'answers',
+        'multiply'
+    ],
+    outputs: [
+        'onChange'
+    ],
+    directives: [
+        QuestionAnswerComponent
+    ],
     template: `
-      <pre>{{answers | json}}</pre>
-  `
+<div class="question-answers-container">
+    <div class="question-answer-option" 
+         *ngFor="let answer of answers">
+        <question-answer [answer]="answer" 
+                         [multiply]="multiply"
+                         (onChange)="handleChange($event)">
+        </question-answer>
+    </div>
+</div>
+`
 })
 export class QuestionAnswersComponent {
-    constructor() {
+    elementType: string = 'radio';
+    elementInputClass: string = 'mdl-radio__button';
+    multiply: string = '';
+    answers: Array<any> = [];
+    onChange: EventEmitter<any> = new EventEmitter();
+
+    constructor(private elementRef: ElementRef) {
     }
 
     ngOnInit() {
+        if (this.multiply) {
+            this.elementType = 'checkbox';
+            this.elementInputClass = 'mdl-checkbox__input';
+        }
+    }
+
+    handleChange(e, v) {
+        let checkedAnwers = this.answers.filter( el => el.checked );
+        console.log(checkedAnwers);
+        this.onChange.emit(checkedAnwers);
     }
 }
