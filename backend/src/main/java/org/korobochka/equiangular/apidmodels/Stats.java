6ffb564fc9fe13421979a4e434035dbbf459d3f6
Lucas Garswood <Lucas.Garswood@kohls.com>
2016-05-15 23:34:04 +0300

    Fixed folder name for routing

--- backend/src/main/java/org/korobochka/equiangular/apidmodels/Stats.java
+++ backend/src/main/java/org/korobochka/equiangular/apidmodels/Stats.java
@@ -1,8 +1,12 @@
 package org.korobochka.equiangular.apidmodels;
 
+import java.util.List;
+
 /**
  * Created by korobochka on 5/15/16.
  */
 public class Stats {
 	public long answersSubmitted = 0;
+
+	//public List<LinkedHashMapSet<S>>
 }
